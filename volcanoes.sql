66fa6013bcec937709212d2653fbe54457433820
Judy Long <long.judy92@gmail.com>
2016-03-03 03:44:06 -0500

    add volcanoes sql file

---
+++ volcanoes.sql
@@ -0,0 +1,51 @@
+CREATE TABLE volcano (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  last_eruption VARCHAR(255) NOT NULL,
+  country_id INTEGER,
+
+  FOREIGN KEY(country_id) REFERENCES country(id)
+);
+
+CREATE TABLE country (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  continent_id INTEGER,
+
+  FOREIGN KEY(continent_id) REFERENCES continent(id)
+);
+
+CREATE TABLE continent (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO
+  continent (id, name)
+VALUES
+  (1, "Europe"),
+  (2, "Asia"),
+  (3, "South America"),
+  (4, "North America"),
+  (5, "Antarctica"),
+  (6, "Australia"),
+  (7, "Africa");
+
+INSERT INTO
+  country (id, name, continent_id)
+VALUES
+  (1, "Italy", 1),
+  (2, "Japan", 2),
+  (3, "Chile", 3),
+  (4, "Mexico", 3),
+  (5, "Iceland", 2);
+
+INSERT INTO
+  volcano (id, name, country_id, last_eruption)
+VALUES
+  (1, "Mount Etna", 1, "December 3, 2015"),
+  (2, "Stromboli", 1, "Ongoing"),
+  (3, "Vesuvius", 1, "1944"),
+  (4, "Fuji", 2, "December 16, 1707"),
+  (5, "Cerro Azul", 3, "1967"),
+  (6, "Colima", 4, "2015");
